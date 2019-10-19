eca37a7d6d9f534cdf09c1519eef2b1b3e121b9b
Milton <seraron.lucarios@gmail.com>
2019-10-19 14:18:56 -0500

    recursividad ++

--- Fundamentos de Programacion/Recursividad/Problema.cpp
+++ Fundamentos de Programacion/Recursividad/Problema.cpp
@@ -1,1 +1,27 @@
-test
+#include <iostream>
+
+void recursivo(int a, int b, int cont, int suma);
+
+int main()
+{
+    int a, b, cont = 1, suma;
+    std::cout << "Digite a: ";
+    std::cin >> a;
+    std::cout << "Digite b: ";
+    std::cin >> b;
+    suma = a+b;
+    recursivo(a,b,cont,suma);
+    system("pause");
+    return 0;
+}
+
+void recursivo(int a, int b, int cont, int  suma)
+{
+    int aux = cont;
+    if (aux <= 10)
+    {
+        std::cout << suma << "  ";
+        aux++;
+        recursivo(a, b, aux, suma+b);
+    }
+}

cce8547121c78514bd532746eab9649c212aba27
Milton <seraron.lucarios@gmail.com>
2019-10-31 02:46:51 -0500

    Ejercicio02.cpp

--- Estructura de Datos/Arboles/Ejercicio02.cpp
+++ Estructura de Datos/Arboles/Ejercicio02.cpp
@@ -10,3 +10,63 @@ struct NodoArbol
 };
 typedef NodoArbol *arbol;
 
+void inserta(arbol &abb, int x);
+void preOrden(arbol abb, int dato);
+bool empty(arbol abb);
+
+int main()
+{
+    arbol abb = NULL;
+    int n, x;
+
+    cout << "Numero de elementos: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Elemento: ";
+        cin >> x;
+        inserta(abb, x);
+    }
+
+    cout << "Ingrese un elemento: ";
+    cin >> x;
+    preOrden(abb, x);
+
+    system("pause");
+    return 0;
+}
+
+void inserta(arbol &abb, int x)
+{
+    if (empty(abb))
+    {
+        abb = new NodoArbol;
+        abb->info = x;
+        abb->hi = NULL;
+        abb->hd = NULL;
+    }
+    else if (x < abb->info)
+        inserta(abb->hi, x);
+    else if (x > abb->info)
+        inserta(abb->hd, x);
+}
+
+void preOrden(arbol abb, int dato)
+{
+    if (!empty(abb))
+    {
+        if(abb->info != dato)
+            cout << abb->info << "  ";
+        else
+            exit(1);
+        preOrden(abb->hi, dato);
+        preOrden(abb->hd, dato);
+    }
+}
+
+bool empty(arbol abb)
+{
+    return (abb == NULL) ? true : false;
+}
+

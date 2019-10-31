9ba9548eb4dbfd93a3b3d3ff96ce06c05a7fc23f
Milton <seraron.lucarios@gmail.com>
2019-10-31 04:21:36 -0500

    ejercicio10.cpp

---
+++ Estructura de Datos/Arboles/Ejercicio10.cpp
@@ -0,0 +1,68 @@
+//Contar el numero de hojas tiene un árbol binario de búsqueda.
+#include <iostream>
+using namespace std;
+
+struct NodoArbol
+{
+    int info;
+    NodoArbol *hi;
+    NodoArbol *hd;
+};
+typedef NodoArbol *arbol;
+
+void inserta(arbol &abb, int x);
+bool empty(arbol abb);
+int hojas(arbol abb);
+
+int main()
+{
+    arbol abb = NULL, b;
+    int n, x;
+
+    cout << "Numero de elementos: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Elemento: ";
+        cin >> x;
+        inserta(abb, x);
+    }
+    
+    cout << "El numero de hojas es: " << hojas(abb) << endl;
+    
+    system("pause");
+    return 0;
+}
+
+void inserta(arbol &abb, int x)
+{
+    if (empty(abb))
+    {
+        abb = new NodoArbol;
+        abb->info = x;
+        abb->hi = NULL;
+        abb->hd = NULL;
+    }
+    else if (x < abb->info)
+        inserta(abb->hi, x);
+    else if (x > abb->info)
+        inserta(abb->hd, x);
+}
+
+int hojas(arbol abb)
+{
+    if (!empty(abb))
+    {
+        if(abb->hi == NULL && abb->hd == NULL)
+            return 1 + hojas(abb->hi) + hojas(abb->hd);
+        else
+            return hojas(abb->hi) + hojas(abb->hd);
+    }
+    return 0;
+}
+
+bool empty(arbol abb)
+{
+    return (abb == NULL) ? true : false;
+}
